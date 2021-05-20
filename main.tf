4204a67aebe8306f312e7aa9ffad3232b65e98ec
davisanc <dsanchezrodriguez@gmail.com>
2021-05-20 14:55:25 +0100

    firewall policy

--- main.tf
+++ main.tf
@@ -49,87 +49,6 @@ resource "azurerm_public_ip" "pip" {
   sku                 = "Standard"
 }
 
-module "application-gateway" {
-  source                    = "aztfm/application-gateway/azurerm"
-  version                   = "1.0.0"
-  name                      = "application-gateway"
-  resource_group_name       = azurerm_resource_group.rg.name
-  location                  = azurerm_resource_group.rg.location
-  sku                       = { tier = "WAF_v2", size = "WAF_v2", capacity = 2 }
-  subnet_id                 = azurerm_subnet.app-gw-subnet.id
-  frontend_ip_configuration = { public_ip_address_id = azurerm_public_ip.pip.id, private_ip_address = "10.0.2.10", private_ip_address_allocation = "Static" }
-  backend_address_pools = [
-    { name = "backend-address-pool-1" },
-    { name = "backend-address-pool-2", ip_addresses = "10.0.0.4,10.0.0.5,10.0.0.6" }
-  ]
-  http_listeners        = [{ name = "http-listener", frontend_ip_configuration = "Public", port = 80, protocol = "http" }]
-  backend_http_settings = [{ name = "backend-http-setting", port = 80, protocol = "http", request_timeout = 20 }]
-  request_routing_rules = [
-    {
-      name                       = "request-routing-rule-1"
-      http_listener_name         = "http-listener"
-      backend_address_pool_name  = "backend-address-pool-1"
-      backend_http_settings_name = "backend-http-setting"
-    },
-    {
-      name                       = "request-routing-rule-2"
-      http_listener_name         = "http-listener"
-      backend_address_pool_name  = "backend-address-pool-2"
-      backend_http_settings_name = "backend-http-setting"
-    }
-  ]
-}
-
-# Create our Azure Storage Account - WAF-DevOp-sa
-resource "azurerm_storage_account" "wafdevopssa" {
-  name                     = "wafdevopssa"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  tags = {
-    environment = "WAF-DevOps"
-  }
-}
-# Create our vNIC for our VM and assign it to our Virtual Machines Subnet
-resource "azurerm_network_interface" "vmnic" {
-  name                = "WAF-DevOp-vm-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
- 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.sn.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-# Create our Virtual Machine - WAF-DevOps-VM01
-resource "azurerm_virtual_machine" "WAF-DevOp-vm01" {
-  name                  = "WAF-DevOp-vm01"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.vmnic.id]
-  vm_size               = "Standard_B2s"
-  storage_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter-Server-Core-smalldisk"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "WAF-DevOpsvm01os"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name      = "WAF-DevOpsvm01"
-    admin_username     = "david"
-    admin_password     = "Sanchez123$%"
-  }
-  os_profile_windows_config {
-  }
-}
 resource "azurerm_web_application_firewall_policy" "WAF-Devops-waf" {
   name                = "WAF-Devops-wafpolicy"
   resource_group_name = azurerm_resource_group.rg.name
@@ -215,4 +134,95 @@ resource "azurerm_web_application_firewall_policy" "WAF-Devops-waf" {
     }
   }
 
+}
+
+data "azurerm_web_application_firewall_policy" "policyexample" {
+  resource_group_name = azurerm_resource_group.rg.name
+  name                = "WAF-Devops-wafpolicy"
+}
+
+output "id" {
+  value = data.azurerm_web_application_firewall_policy.policyexample.id
+}
+
+module "application-gateway" {
+  source                    = "aztfm/application-gateway/azurerm"
+  version                   = "1.0.0"
+  name                      = "application-gateway"
+  resource_group_name       = azurerm_resource_group.rg.name
+  location                  = azurerm_resource_group.rg.location
+  sku                       = { tier = "WAF_v2", size = "WAF_v2", capacity = 2 }
+  subnet_id                 = azurerm_subnet.app-gw-subnet.id
+  frontend_ip_configuration = { public_ip_address_id = azurerm_public_ip.pip.id, private_ip_address = "10.0.2.10", private_ip_address_allocation = "Static" }
+  backend_address_pools = [
+    { name = "backend-address-pool-1" },
+    { name = "backend-address-pool-2", ip_addresses = "10.0.0.4,10.0.0.5,10.0.0.6" }
+  ]
+  http_listeners        = [{ name = "http-listener", frontend_ip_configuration = "Public", port = 80, protocol = "http", firewall_policy_id = data.azurerm_web_application_firewall_policy.policyexample.id }]
+  backend_http_settings = [{ name = "backend-http-setting", port = 80, protocol = "http", request_timeout = 20 }]
+  request_routing_rules = [
+    {
+      name                       = "request-routing-rule-1"
+      http_listener_name         = "http-listener"
+      backend_address_pool_name  = "backend-address-pool-1"
+      backend_http_settings_name = "backend-http-setting"
+    },
+    {
+      name                       = "request-routing-rule-2"
+      http_listener_name         = "http-listener"
+      backend_address_pool_name  = "backend-address-pool-2"
+      backend_http_settings_name = "backend-http-setting"
+    }
+  ]
+}
+
+# Create our Azure Storage Account - WAF-DevOp-sa
+resource "azurerm_storage_account" "wafdevopssa" {
+  name                     = "wafdevopssa"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags = {
+    environment = "WAF-DevOps"
+  }
+}
+# Create our vNIC for our VM and assign it to our Virtual Machines Subnet
+resource "azurerm_network_interface" "vmnic" {
+  name                = "WAF-DevOp-vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+ 
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sn.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+# Create our Virtual Machine - WAF-DevOps-VM01
+resource "azurerm_virtual_machine" "WAF-DevOp-vm01" {
+  name                  = "WAF-DevOp-vm01"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.vmnic.id]
+  vm_size               = "Standard_B2s"
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter-Server-Core-smalldisk"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "WAF-DevOpsvm01os"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name      = "WAF-DevOpsvm01"
+    admin_username     = "david"
+    admin_password     = "Sanchez123$%"
+  }
+  os_profile_windows_config {
+  }
 }
