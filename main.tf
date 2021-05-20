aea33c5bc0d11f6e38945a776f01189c85501021
davisanc <dsanchezrodriguez@gmail.com>
2021-05-20 14:00:11 +0100

    subnet change

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "azurerm_subnet" "sn" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes       = ["10.0.1.0/24"]
 }
-resource "azurerm_subnet" "sn" {
+resource "azurerm_subnet" "app-gw-subnet" {
   name                 = "app-gw-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
