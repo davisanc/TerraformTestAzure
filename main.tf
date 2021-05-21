69cbd658c2ba58472297eee4c926671be68ce41a
davisanc <dsanchezrodriguez@gmail.com>
2021-05-21 22:28:38 +0100

    change version number

--- main.tf
+++ main.tf
@@ -9,7 +9,8 @@ terraform {
     azurerm = {
       # Specify what version of the provider we are going to utilise
       source = "hashicorp/azurerm"
-      version = ">= 2.4.1"
+      #version = ">= 2.4.1"
+      version = 0.12.3
     }
   }
 }
