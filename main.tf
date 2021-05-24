af1c483d9e70018ef992e24d455001c7ca92628b
davisanc <dsanchezrodriguez@gmail.com>
2021-05-24 13:19:45 +0100

    change storage account for app

--- main.tf
+++ main.tf
@@ -173,8 +173,8 @@ module "application-gateway" {
 }
 
 # Create our Azure Storage Account - WAF-DevOp-sa
-resource "azurerm_storage_account" "wafdevopssa" {
-  name                     = "wafdevopssa"
+resource "azurerm_storage_account" "wafdevopssa-app" {
+  name                     = "wafdevopssa-app"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
