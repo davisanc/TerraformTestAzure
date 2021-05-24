798140aa17f838e1621226751578754b0a351ac3
davisanc <dsanchezrodriguez@gmail.com>
2021-05-24 17:17:45 +0100

    change CIDR

--- attachWAFpolicy.tf
+++ attachWAFpolicy.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  address_space       = ["10.1.0/16"]
+  address_space       = ["10.1.0.0/16"]
 }
 
 resource "azurerm_subnet" "frontend" {
