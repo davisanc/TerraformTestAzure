cd9985d7e9209ad656672b971fa7b949b911b20e
davisanc <dsanchezrodriguez@gmail.com>
2021-05-24 13:48:54 +0100

    change to waf policy ID

--- getwafpolicy.tf
+++ getwafpolicy.tf
@@ -1,6 +1,6 @@
 data "azurerm_web_application_firewall_policy" "example" {
-  resource_group_name = "existing"
-  name                = "existing"
+  resource_group_name = "WAF-DevOps-app"
+  name                = "WAF-Devops-wafpolicy"
 }
 
 output "id" {
