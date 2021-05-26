ba93fa5822f99d8db0fd4199693009a896ea36e0
davisanc <dsanchezrodriguez@gmail.com>
2021-05-26 13:14:15 +0100

    adding kind is linux for app server

--- createJuiceShopWebApp.tf
+++ createJuiceShopWebApp.tf
@@ -2,6 +2,8 @@ resource "azurerm_app_service_plan" "example" {
   name                = "juiceshop-appserviceplan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  kind = "Linux"
+  reserved = true
 
   sku {
     tier = "Standard"
