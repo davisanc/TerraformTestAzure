6a040b56b2e729c920c1d5693521fee93171250f
davisanc <dsanchezrodriguez@gmail.com>
2021-05-26 11:39:52 +0100

    add file to create juiceshop webapp

---
+++ createJuiceShopWebApp.tf
@@ -0,0 +1,27 @@
+resource "azurerm_app_service_plan" "example" {
+  name                = "juiceshop-appserviceplan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "juiceshop-app-service"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    linux_fx_version = "DOCKER|mohitkusecurity/juice-shop-updated"
+    always_on = true
+  }
+
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+}
