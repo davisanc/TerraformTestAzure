4e26fa16674c25d2c8965c30184613575637ae77
davisanc <dsanchezrodriguez@gmail.com>
2021-05-26 13:00:56 +0100

    change to not use variables

--- createJuiceShopWebApp.tf
+++ createJuiceShopWebApp.tf
@@ -15,13 +15,12 @@ resource "azurerm_app_service" "example" {
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.example.id
 
-  site_config {
-    linux_fx_version = "DOCKER|${var.docker_image}:${var.docker_image_tag}"
-    always_on = true
-  }
-
   app_settings = {
     "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+   site_config {
+    linux_fx_version = "DOCKER|mohitkusecurity/juice-shop-updated:latest"
+    always_on = true
   }
 }
