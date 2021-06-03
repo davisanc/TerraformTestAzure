48c42a675b13ebcfc347e4e156edede969587392
davisanc <dsanchezrodriguez@gmail.com>
2021-06-03 14:13:39 +0100

    add comments

--- getwafpolicy.tf
+++ getwafpolicy.tf
@@ -1,8 +1,8 @@
-data "azurerm_web_application_firewall_policy" "example" {
-  resource_group_name = "WAF-DevOps-app"
-  name                = "WAF-Devops-wafpolicy"
-}
+##data "azurerm_web_application_firewall_policy" "example" {
+  ##resource_group_name = "WAF-DevOps-app"
+  ##name                = "WAF-Devops-wafpolicy"
+##}
 
-output "id" {
-  value = data.azurerm_web_application_firewall_policy.example.id
-}
+##output "id" {
+  ##value = data.azurerm_web_application_firewall_policy.example.id
+##}
