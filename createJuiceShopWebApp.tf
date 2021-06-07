b2ed7b68792147b966753b7db516fa163f1a7eaa
davisanc <dsanchezrodriguez@gmail.com>
2021-06-07 11:19:05 +0100

    change web app name

--- createJuiceShopWebApp.tf
+++ createJuiceShopWebApp.tf
@@ -19,7 +19,8 @@ resource "random_id" "webappname" {
 resource "azurerm_app_service" "example" {
   #"[concat('owaspdirect','-', uniqueString(resourceGroup().id))]"
   #name                = "juiceshop-app-service"
-  name                = "[concat("juiceshop","-","random_id.webappname")]"         
+  #name                = "[concat("juiceshop","-","random_id.webappname")]"         
+  name                = "juiceshop ${random_id.webappname.hex}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.example.id
